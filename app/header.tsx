f8bc0eed497aa2bc082b22ea41f51744a8414372
niemiaszek <niemiecpatryk@outlook.com>
2023-04-17 03:00:58 +0200

    Don't prefetch to let the site to revalidate

--- app/header.tsx
+++ app/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="sticky top-0 min-w-full bg-gray-300  py-3 md:py-5">
       <div className="flex flex-row text-2xl md:text-3xl text-gray-700 text-center justify-center space-x-5">
-        <Link href="/">
+        <Link href="/" prefetch={false}>
           <h1 className="font-bold mx-2">FIFO</h1>
         </Link>
         <h1 className="mx-2">Kantor Wymiany Walut</h1>
