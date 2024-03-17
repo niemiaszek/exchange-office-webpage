618bf9eda2f0f74395a1f7281d9dac07613ff07b
Patryk Niemiec <1napiszdopatryka8@gmail.com>
2024-03-17 23:20:43 +0100

    Add Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import Header from "./header";
 import Providers from "./providers";
 import localFont from "next/font/local";
@@ -39,6 +41,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
